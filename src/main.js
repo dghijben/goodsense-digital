c5ef91c7876da7bf735e9e9591b11f46c4aadc6a
dghijben <dghijben@gmail.com>
2015-10-22 15:19:50 +0900

    initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+
+var Route = ReactRouter.Route;
+
+var routes = (<Route handler={require('./components/App')}> </Route>);
+
+ReactRouter.run(routes, ReactRouter.HistoryLocation, function (Root) {
+  React.render(<Root />, document.getElementById('app'));
+})
+
+var events = require('events');
+console.log(events);
+
+console.log('hello world');
