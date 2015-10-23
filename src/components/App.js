24648e75075288e88826526c64fda63a0b2953b9
dghijben <dghijben@gmail.com>
2015-10-23 16:49:55 +0900

    added side nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,56 @@
 var React = require('react');
 var RouteHandler = require('react-router').RouteHandler;
-const AppBar = require('material-ui/lib/app-bar');
+var AppBar = require('material-ui/lib/app-bar');
+var LeftNav = require('material-ui/lib/left-nav');
+var IconButton = require('material-ui/lib/icon-button');
+var IconMenu = require('material-ui/lib/menus/icon-menu');
+var MenuItem = require('material-ui/lib/menus/menu-item');
+var NavigationMoreVert = require('material-ui/lib/svg-icons/navigation/more-vert');
+var injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
+
+var menuItems = [
+  {
+    text: 'Resources'
+  },
+  {
+     text: 'GitHub'
+  },
+  {
+     text: 'Disabled',
+     disabled: true
+  },
+  {
+     text: 'Disabled Link',
+     disabled: true
+  },
+];
 
 var App = React.createClass({
 
   render: function () {
     return (
       <div>
-        <AppBar
-          title="Title"
-          iconClassNameRight="muidocs-icon-navigation-expand-more" />
+      <AppBar
+        title="Title"
+        onLeftIconButtonTouchTap={this._toggleNav}
+        iconElementRight={
+          <IconMenu iconButtonElement={
+            <IconButton><NavigationMoreVert /></IconButton>
+          }>
+            <MenuItem primaryText="Refresh" />
+            <MenuItem primaryText="Help" />
+            <MenuItem primaryText="Sign out" />
+          </IconMenu>
+        } />
+
+        <LeftNav ref="leftNav" docked={false} menuItems={menuItems} />
         <RouteHandler />
       </div>
     )
+  },
+  _toggleNav() {
+    this.refs.leftNav.toggle();
   }
 });
 
